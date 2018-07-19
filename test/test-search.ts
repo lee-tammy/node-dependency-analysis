c4e3f52e99f48a9af78f85042e5839a1b87b7f5f
Tammy Lee <ltammy@google.com>
2018-07-18 17:15:10 -0700

    Fixed some typos and added more test cases

--- test/test-search.ts
+++ test/test-search.ts
@@ -1,37 +1,52 @@
 import test from 'ava';
 import * as search from '../src/search';
 
-// standard require
+// standard require case
 test(
     'search on file with http request should return a searchValue object with correct fields',
     async t => {
-      const content = 'const require(\'http\');';
+      const content = 'const a = require(\'http\');';
       const result = await search.search(content);
       t.true(result.http === true);
-    });
+    }
+);
 
-// require in a variable
+// require callee is defined as a variable
 test(
     'search on file with http request(require in a var) should return a searchValue object with correct fields',
     async t => {
       const content = 'const r = require;\n something = r(\'http\');';
       const result = await search.search(content);
       t.true(result.http === true);
-    });
+    }
+);
 
-// concatenating http string
+// require arg is a concatenation of strings that forms http 
 test(
     'search on file with http request(concatenated http string) should return a searchValue object with correct fields',
     async t => {
-      const content = 'const require(\'htt + p\');';
+      const content = 'const a = require(\'h\' + \'t\' + \'t\' + \'p\');';
       const result = await search.search(content);
       t.true(result.http === true);
-    });
+    }
+);
+
+// require arg is a substring that forms http
+test(
+    'search on file with http request(concatenated http string) should return a searchValue object with correct fields',
+    async t => {
+        const content = 'const a = require(\'h\' + \'t\' + \'t\' + \'p\');';
+        const result = await search.search(content);
+        t.true(result.http === true);
+    }
+);
+
 
 test(
     'search on file without http request should return a searchValue object with correct fields',
     async t => {
-      const content = 'const require(\'meow\');';
+      const content = 'const a=  require(\'meow\');';
       const result = await search.search(content);
       t.true(result.http === false);
-    });
+    }
+);
