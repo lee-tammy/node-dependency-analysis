9ce92dee54d4d08dff41c96396c0ec521900abe9
Tammy Lee <ltammy@google.com>
2018-08-07 00:19:12 -0700

    implemented some part of getPackagePOIList

--- src/package-tree.ts
+++ src/package-tree.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import { X_OK } from 'constants';
+import { getDynamicEval, getIOModules } from './analysis';
 
 export interface PointOfInterest {
   type: string;
@@ -43,27 +45,47 @@ function getPOIforPackageTree(packageTree: PackageTree): PackageTree {
 }
 
 // Should the parameter be a list of jsfiles instead of a packagetree
-function getPackagePOIList(pkg: PackageTree): PointOfInterest[] {
-  // TODO:
+//parameter = pkg: PackageTree
+export function getPackagePOIList(): PointOfInterest[] {
+  const packagePOIList: PointOfInterest[] = [];
   // calls getPointsOfInterest for each file in package
   // step 1: Locate the package.json/ module folder file for this package
-  // step 2: for each file{
-  // get the POI array
-  // concatenate it with a result array
-  // return the result
-  throw new Error('not implemented');
+  // step 2: for each file => 
+  //                      get the POI array
+  //                      concatenate it with a result array
+  const files = getJSFiles('./node_modules/tslint/');
+  files.forEach((file) => {
+    const content = fs.readFileSync(file, 'utf8').toString();
+    const functionArr: Function[] = [getIOModules, getDynamicEval];
+    const filePOIList = getPointsOfInterest(content, file, functionArr);
+    packagePOIList.push(...filePOIList);
+  });
+  
+  // step 3: return the result array
+  return packagePOIList;
 }
+
+/*export function findPackageDir(name: string, version: string): string{
+  const moduleFolders: string[] = fs.readdirSync('./node_modules/', 'utf8');
+  moduleFolders.find((mod) => {
+
+    if(mod === name){
+
+    }
+  })
+  return '';
+}*/
 
 // Gets the path of all js files
 export function getJSFiles(dir: string): string[] {
   const topLevelFiles = fs.readdirSync(dir);
   const fileList: string[] = [];
   topLevelFiles.forEach((file) => {
-    if (file.endsWith('.js') || file.endsWith('.ts')) {
+    if (file.endsWith('.js')) {
       fileList.push(`${dir}${file}`);
-    } else if (fs.statSync(`${dir}/${file}`).isDirectory()) {
-      const path = `${dir}${file}`;
-      fileList.push(`${getJSFiles(path)}`);
+    } else if (fs.statSync(`${dir}${file}`).isDirectory()) {
+      const path = `${dir}${file}/`;
+      fileList.push(...getJSFiles(path));
     }
   });
   return fileList;
