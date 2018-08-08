444c4f0bac1e8f70113a5659386a53360170415a
Tammy Lee <ltammy@google.com>
2018-08-07 21:57:29 -0700

    Added variable

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
-import pify from 'pify';
 import fs from 'fs';
+import pify from 'pify';
 
 export const filesInDir = pify(fs.readdir);
 export const readFile = pify(fs.readFile);
-export const fileInfo = pify(fs.stat)
+export const fileInfo = pify(fs.stat);
