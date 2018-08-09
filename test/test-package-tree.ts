9e7a51de66107d0ffccf0f2ade13ea62de69199e
Tammy Lee <ltammy@google.com>
2018-08-09 11:33:39 -0700

    Started on testing

--- test/test-package-tree.ts
+++ test/test-package-tree.ts
@@ -1,24 +1,9 @@
 import { TestProject } from "./util";
 import test from 'ava';
-import pify from 'pify';
-import fs from 'fs';
 import * as tree from '../src/package-tree';
 import {PackageTree} from './util-package-tree';
 
 test('first tester', async t => {
-
-    const test1 = new TestProject({
-        '*': ['a@1', 'c@2', 'd@1'],
-        'a@1': ['b@1', 'c@1'],
-        'b@1': ['c@1'],
-        'c@1': [],
-        'c@2': [],
-        'd@1': []
-    })
-    const path1: string = await test1.create();
-    const root = new PackageTree<null>('a', '1.0.0', null, []);
-    const resolvedTree:PackageTree<string> = tree.resolvePaths(root, path1);
-    console.log(resolvedTree.name);
 
     const test = new TestProject({
         '*': ['a@1', 'c@2', 'd@1'],
@@ -29,12 +14,15 @@ test('first tester', async t => {
         'd@1': []
     })
     const path: string = await test.create();
-    const readDirA = await pify(fs.readdir)(path);
-    console.log('path to *: ' + readDirA)
-    const readDirB = await pify(fs.readdir)(`${path}/node_modules/a`);
-    console.log('path to b: ' + readDirB)
-    const readDir2 = await pify(fs.readdir)(`${path}/node_modules/a/node_modules/c`);
-    console.log(readDir2);
-    test.cleanup();
+
+    const d1 = new PackageTree<null>('d', '1.0.0', null, []);
+    const c1 = new PackageTree<null>('c', '1.0.0', null, []);
+    const c2 = new PackageTree<null>('c', '1.0.0', null, []);
+    const b1 = new PackageTree<null>('b', '1.0.0', null, [c1]);
+    const a1 = new PackageTree<null>('a', '1.0.0', null, [b1, b1]);
+    const root = new PackageTree<null>('root', '1.0.0', null, [a1, c2, d1]);
+
+    const resolvedTree:PackageTree<string> = tree.resolvePaths(root, path);
+    console.log(JSON.stringify(resolvedTree, null, 2));
 });
 
