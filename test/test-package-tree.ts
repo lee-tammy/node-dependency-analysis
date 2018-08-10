1abf913f9e9ab1cde4be64c404d551b2f3daa8ca
Tammy Lee <ltammy@google.com>
2018-08-10 10:46:20 -0700

    Added first test

--- test/test-package-tree.ts
+++ test/test-package-tree.ts
@@ -1,25 +1,68 @@
 import test from 'ava';
+import path from 'path';
 
-import * as tests from './test-projects'
 import * as tree from '../src/package-tree';
+import {PackageTree} from '../src/package-tree';
 
-import {PackageTree} from './util-package-tree';
+import * as tests from './mock-projects';
 
-test('the data property of the package tree should changed from null ' + 
-    'to an array of Points of Interest', async t => {
-    const path: string = await tests.test1.create();
+test(
+    'the data property of the package tree should changed from null ' +
+        'to an array of Points of Interest',
+    async t => {
+      // See how the dependency graph looks in test-projects
+      const testPath: string = await tests.test1.create();
 
-    const d1 = new PackageTree<null>('d', '1.0.0', null, []);
-    const c1 = new PackageTree<null>('c', '1.0.0', null, []);
-    const c2 = new PackageTree<null>('c', '2.0.0', null, []);
-    const b1 = new PackageTree<null>('b', '1.0.0', null, [c1]);
-    const a1 = new PackageTree<null>('a', '1.0.0', null, [b1, c1]);
-    const root = new PackageTree<null>('root', '1.0.0', null, [a1, c2, d1]);
-  
-    console.log('before: ' + JSON.stringify(root, null, 2));
-    const resolvedTree: PackageTree<string> = await tree.resolvePaths(root, path);
-    console.log('after: ' + JSON.stringify(resolvedTree, null, 2));
-  
-    tests.test1.cleanup();
-    t.pass();
+      const c1 = {name: 'c', version: '1.0.0', data: null, dependencies: []};
+      const c2 = {name: 'c', version: '2.0.0', data: null, dependencies: []};
+      const b1 = {name: 'b', version: '1.0.0', data: null, dependencies: [c1]};
+      const a1 =
+          {name: 'a', version: '1.0.0', data: null, dependencies: [b1, c1]};
+      const root =
+          {name: 'root', version: '1.0.0', data: null, dependencies: [a1, c2]};
+
+      const resolvedTree: PackageTree<string> =
+          await tree.resolvePaths(root, testPath);
+
+      const rootDependencies = resolvedTree.dependencies.sort(sortByName);
+      const a1Pjson =
+          require(path.join(rootDependencies[0].data, 'package.json'));
+      t.deepEqual(a1Pjson.name, 'a');
+      t.deepEqual(a1Pjson.version, '1.0.0');
+      const c2Pjson =
+          require(path.join(rootDependencies[1].data, 'package.json'));
+      t.deepEqual(c2Pjson.name, 'c');
+      t.deepEqual(c2Pjson.version, '2.0.0');
+
+      const a1Dependencies = rootDependencies[0].dependencies.sort(sortByName);
+      const b1Pjson =
+          require(path.join(a1Dependencies[0].data, 'package.json'));
+      t.deepEqual(b1Pjson.name, 'b');
+      t.deepEqual(b1Pjson.version, '1.0.0');
+      const c1Pjson1 =
+          require(path.join(a1Dependencies[1].data, 'package.json'));
+      t.deepEqual(c1Pjson1.name, 'c');
+      t.deepEqual(c1Pjson1.version, '1.0.0');
+
+      const b1Dependencies = a1Dependencies[0].dependencies.sort(sortByName);
+      const c1Pjson2 =
+          require(path.join(b1Dependencies[0].data, 'package.json'));
+      t.deepEqual(c1Pjson2.name, c1Pjson2.name);
+      t.deepEqual(c1Pjson2.version, c1Pjson2.version);
+
+      tests.test1.cleanup();
+    });
+
+test('TODO: test2', async t => {
+  // See how the dependency graph looks in test-projects
+  const testPath: string = await tests.test2.create();
+  tests.test2.cleanup();
 });
+
+function sortByName(a: PackageTree<string>, b: PackageTree<string>) {
+  if (a.name < b.name) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
